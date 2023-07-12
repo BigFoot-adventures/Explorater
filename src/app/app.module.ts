fa14607d61a50e6f5764d6a732108b29deabf60f
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-07-12 15:57:54 -0400

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { AppendHtmlDirective } from './directives/append-html.directive';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     HttpClientModule, 
     FormsModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  serverEndpoint: 'https://wfa3.josecgomez.dev'
 };
